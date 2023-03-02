286e98d2a6821a3b1201332b532490e84bf38165
Khoirul Bahra <107555389+MyMasWayVPN@users.noreply.github.com>
2023-03-03 01:22:34 +0700

    Update Tarap-Kuhing.sh

--- ssh/Tarap-Kuhing.sh
+++ ssh/Tarap-Kuhing.sh
@@ -2,11 +2,11 @@
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 
-DOMAIN=mwvpn.tech
+DOMAIN=indossh.ninja
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 #subsl=$(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.mwvpn.tech
-#NS_dns=${subsl}.mwvpn.tech
+dns=${sub}.indossh.ninja
+#NS_dns=${subsl}.indossh.ninja
 CF_ID=akunabal.abal7770@gmail.com
 CF_KEY=4502348bc050806208bb10e3a1af5b9d1d018
 set -euo pipefail
